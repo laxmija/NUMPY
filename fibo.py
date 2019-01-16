42b9094337981e74da8d0619c688a95314f19a5f
laxmija <45327851+laxmija@users.noreply.github.com>
2019-01-16 19:02:13 +0530

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 18:45:33 2019
+
+@author: Laxmi
+"""
+
+# Fibonacci numbers module
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n): # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+    return result
